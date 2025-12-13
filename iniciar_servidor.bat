5a6d7a0dbc5f5f190bd2a73f9b3d95e5fd83e1de
Grackoo <jonnathanjosue@outlook.com>
2025-12-12 19:38:36 -0600

    awdas

---
+++ iniciar_servidor.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Iniciando servidor local para SIA TEC...
+echo.
+echo Por favor, abre tu navegador y ve a: http://localhost:8000
+echo.
+echo (Mantén esta ventana abierta mientras uses la página)
+echo.
+python -m http.server 8000
+pause
